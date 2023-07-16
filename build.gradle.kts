f9f42777c613eddf30d3694c21876310aab34ad1
Chris Lee <chris@cloudshiftconsulting.com>
2023-07-16 14:15:31 -0700

    Updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,12 @@ plugins {
     id("io.github.gradle-nexus.publish-plugin") version("2.0.0-rc-1")
 }
 
-configure<NexusPublishExtension> {
+nexusPublishing {
     this.repositories {
-        sonatype()
-//        {  //only for users registered in Sonatype after 24 Feb 2021
-//            nexusUrl = uri("https://s01.oss.sonatype.org/service/local/")
-//            snapshotRepositoryUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-//        }
+        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
+            nexusUrl = uri("https://s01.oss.sonatype.org/service/local/")
+            snapshotRepositoryUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        }
     }
 }
 
