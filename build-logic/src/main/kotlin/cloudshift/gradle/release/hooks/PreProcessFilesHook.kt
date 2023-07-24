60f171602a889549f65bca1d802b1ffd764c82c1
Chris Lee <chris@cloudshiftconsulting.com>
2023-07-23 18:20:19 -0700

    [Release] - release commit: 0.3.10-SNAPSHOT -> 0.3.10

--- build-logic/src/main/kotlin/cloudshift/gradle/release/hooks/PreProcessFilesHook.kt
+++ build-logic/src/main/kotlin/cloudshift/gradle/release/hooks/PreProcessFilesHook.kt
@@ -5,15 +5,13 @@ package cloudshift.gradle.release.hooks
 import cloudshift.gradle.release.tasks.PreReleaseHook
 import com.google.common.hash.Hashing
 import io.github.z4kn4fein.semver.Version
-import org.aspectj.weaver.tools.cache.SimpleCacheFactory.path
 import org.gradle.api.file.FileSystemOperations
+import org.gradle.api.file.FileTree
 import org.gradle.api.file.ProjectLayout
-import org.gradle.api.internal.file.copy.DefaultCopySpec
 import org.gradle.api.logging.Logging
 import org.gradle.api.model.ObjectFactory
-import org.gradle.kotlin.dsl.newInstance
 import java.io.File
-import java.util.*
+import java.util.UUID
 import javax.inject.Inject
 
 internal abstract class PreProcessFilesHook @Inject constructor(
@@ -66,33 +64,17 @@ internal abstract class PreProcessFilesHook @Inject constructor(
 
             val sourceFileTree = objects.fileTree()
                 .from(templateSpec.templateDir)
+            val sourceDir = sourceFileTree.dir
             val destDir = objects.fileTree()
                 .from(templateSpec.destinationDir).dir
 
             val sourceFiles = sourceFileTree.matching {
                 include(templateSpec.includes)
                 exclude(templateSpec.excludes)
-                if (templateSpec.preventTampering) {
-                    exclude("**/*.sha256")
-                }
+                exclude("**/*.sha256")
             }
 
-            if(templateSpec.preventTampering) {
-                sourceFiles.forEach { file ->
-                    val srcRelative = file.relativeTo(sourceFileTree.dir)
-                    val destFile = destDir.resolve(srcRelative)
-
-                    logger.info("Checking tampering $srcRelative")
-                    // if .sha256 file exists validate that to see if generated content has been tampered with
-                    val srcSha256File = file.parentFile.resolve("${file.name}.sha256")
-
-                    when {
-                        !srcSha256File.exists() -> return@forEach
-                        !destFile.exists() -> return@forEach
-                        destFile.sha256() != srcSha256File.readText() -> error("$destFile tampered with; please delete and do edits in $file")
-                    }
-                }
-            }
+            checkTampering(templateSpec.preventTampering, sourceFiles, sourceDir, destDir)
 
             fs.copy {
                 from(sourceFiles)
@@ -103,14 +85,45 @@ internal abstract class PreProcessFilesHook @Inject constructor(
             }
 
             // put sha256 of expanded content in .sha256 file alongside template, for validation on subsequent releases
-            if (templateSpec.preventTampering) {
-                sourceFiles.forEach { file ->
-                    val srcSha256File = file.parentFile.resolve("${file.name}.sha256")
+            writeTamperChecksum(templateSpec.preventTampering, sourceFiles, sourceDir, destDir)
+        }
+    }
 
-                    val relative = file.relativeTo(sourceFileTree.dir)
-                    val destFile = destDir.resolve(relative)
-                    srcSha256File.writeText(destFile.sha256())
-                }
+    private fun writeTamperChecksum(
+        preventTampering: Boolean,
+        sourceFiles: FileTree,
+        sourceDir : File,
+        destDir: File
+    ) {
+        if (!preventTampering) return
+        sourceFiles.forEach { file ->
+            val srcSha256File = file.parentFile.resolve("${file.name}.sha256")
+
+            val relative = file.relativeTo(sourceDir)
+            val destFile = destDir.resolve(relative)
+            srcSha256File.writeText(destFile.sha256())
+        }
+    }
+
+    private fun checkTampering(
+        preventTampering: Boolean,
+        sourceFiles: FileTree,
+        sourceDir: File,
+        destDir: File
+    ) {
+        if (!preventTampering) return
+        sourceFiles.forEach { file ->
+            val srcRelative = file.relativeTo(sourceDir)
+            val destFile = destDir.resolve(srcRelative)
+
+            logger.info("Checking tampering on $srcRelative")
+            // if .sha256 file exists validate that to see if generated content has been tampered with
+            val srcSha256File = file.parentFile.resolve("${file.name}.sha256")
+
+            when {
+                !srcSha256File.exists() -> return@forEach
+                !destFile.exists() -> return@forEach
+                destFile.sha256() != srcSha256File.readText() -> error("$destFile tampered with; please delete and do edits in $file")
             }
         }
     }
